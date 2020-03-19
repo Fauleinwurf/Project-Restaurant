94d130dd60419fb04e4f6feb33c8fd2b8d147b78
Fauleinwurf <nino.nufrio@outlook.de>
2020-03-19 15:19:16 +0100

    kjh

--- PHP/CLASSES/Restaurant.php
+++ PHP/CLASSES/Restaurant.php
@@ -6,11 +6,11 @@ class Restaurant
     private string $place;
     private int $phoneNumber;
     private int $zip;
+    private int $website;
     private array $menu;
     private string $imagePath;
     private string $id;
     private bool $validated;
-
 
     public function __construct()
     {
@@ -24,6 +24,27 @@ class Restaurant
 
     public function isValidated(): bool
     {
-        return $this->validated;
+        if ($this->validated == true) {
+            return true;
+        }
+        return false;
     }
+
+    public static function createFromArray(array $restaurantData): Restaurant
+    {
+        $restaurant = New Restaurant();
+
+        $restaurant->address = $restaurantData['address'];
+        $restaurant->place = $restaurantData['place'];
+        $restaurant->zip = $restaurantData['zip'];
+        $restaurant->website = $restaurantData['website'];
+        $restaurant->phoneNumber = $restaurantData['phoneNumber'];
+        $restaurant->menu = $restaurantData['menu'];
+        $restaurant->imagePath = $restaurantData['imagePath'];
+        $restaurant->validated = false;
+
+        return $restaurant;
+    }
+
+
 }
