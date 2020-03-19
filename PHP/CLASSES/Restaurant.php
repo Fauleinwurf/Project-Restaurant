cd15c28ebc49a20fa9db4c52f25925b3c554cd82
Fauleinwurf <nino.nufrio@outlook.de>
2020-03-19 13:35:01 +0100

    sdf

--- PHP/CLASSES/Restaurant.php
+++ PHP/CLASSES/Restaurant.php
@@ -1,7 +1,30 @@
 <?php
 
-
 class Restaurant
 {
+    private string $address;
+    private string $place;
+    private int $phoneNumber;
+    private int $zip;
+    private array $menu;
+    private string $imagePath;
+    private string $id;
+    private bool $validated;
 
+
+    public function __construct()
+    {
+        $this->id = uniqid();
+    }
+
+
+    public function changeValidated(): void
+    {
+        $this->validated = !$this->validated;
+    }
+
+    public function isValidated(): bool
+    {
+        return $this->validated;
+    }
 }
