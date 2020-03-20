94d4b0ca2aabe0d6c3876f960221489255667fd0
vitus <linus.lindemann@lernende.bbw.ch>
2020-03-20 09:51:35 +0100

    edit index.html

--- PHP/CLASSES/index.php
+++ PHP/CLASSES/index.php
@@ -6,32 +6,27 @@
     <div class='card mb-5 border border-success'>
         <div class='row'>
             <div class='col-lg-4'>
-                <img class='card-img-top rounded' width='10%' src='' alt='Error'>
+                <img class='card-img-top rounded' width="100%" height="50%" src='' alt='Logo'>
+                <iframe src="https://maps.google.com/maps?q=Elgg&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                        frameborder="0"
+                        allowfullscreen width="100%" height="50%">
+                </iframe>
             </div>
             <div class='col-lg-8'>
                 <div class='card-body'>
-                    <h4 class='card-title'>Test</h4>
+                    <h4 class='card-title'>Name des Restaurants</h4>
                 </div>
                 <ul class='list-group list-group-flush'>
-                    <li class='list-group-item'><strong>Beschreibung:</strong><br>
+                    <li class='list-group-item'><strong>Adresse:</strong><br>
                         <p>Text</p></li>
-                    <li class='list-group-item'>
-                        <table class=table table-bordered>
-                            <thead>
-                            <tr>
-                                <th scope='col'>Vorführungsdatum:</th>
-                                <th scope='col'>Text</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope='row'>Vorführungszeit:</th>
-                                <th>Test</th>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <a class='btn btn-success'>Zur Webseite</a>
-                    </li>
+                    <li class='list-group-item'><strong>Ort:</strong><br>
+                        <p>Text</p></li>
+                    <li class='list-group-item'><strong>PLZ:</strong><br>
+                        <p>Text</p></li>
+                    <li class='list-group-item'><strong>Telefon:</strong><br>
+                        <p><a href="tel:">Telefonnummer</a></p></li>
+                    <li class='list-group-item'><strong>Menu</strong><br>
+                        <p>Text</p></li>
                 </ul>
             </div>
         </div>
